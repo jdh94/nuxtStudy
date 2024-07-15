e357a96722bf3da7f1b2f16f709b47a1399aeb74
jdh94 <pnewlife@naver.com>
2024-07-15 23:01:58 +0900

    초기 환경세팅prettier, es-lint, quasar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2024-07-15',
   devtools: { enabled: true },
   typescript: {
-    shim: false
-  }
-})
-
-
+    shim: false,
+  },
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    /* */
+  },
+});
